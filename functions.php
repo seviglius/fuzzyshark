c9ad3ecba0c054fc04241d1f57dccaee570a2dfb
alluringlights <alluringlights@gmail.com>
2016-04-14 11:43:11 -0700

    Added local bootstrap and enqueued js

--- functions.php
+++ functions.php
@@ -115,15 +115,13 @@ add_action( 'widgets_init', 'portshowlio_theme_widgets_init' );
  */
 function portshowlio_theme_scripts() {
 
-	wp_enqueue_style( 'bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css' );
-
 	wp_enqueue_style( 'portshowlio-theme-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'portshowlio-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'portshowlio-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js' );
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
