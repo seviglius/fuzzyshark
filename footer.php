d499703bbdb0dd9235eb0c7ae9283160bf4784d2
Brenton <alluringlights@gmail.com>
2016-04-21 13:37:23 -0700

    deleted _s code from footer

--- footer.php
+++ footer.php
@@ -11,17 +11,6 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'fuzzyshark' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'fuzzyshark' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'fuzzyshark' ), 'fuzzyshark', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
 <?php wp_footer(); ?>
 
 </body>
