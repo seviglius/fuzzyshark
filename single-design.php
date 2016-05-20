b750b1b5b19ad8a27da18c8013d9a95012cdb7d6
Brenton <alluringlights@gmail.com>
2016-05-19 18:44:51 -0700

    Revert "Revert "test""
    This reverts commit 8e1289f262ee58879b560d623f8c9866988b61ee.

--- single-design.php
+++ single-design.php
@@ -258,8 +258,10 @@
 				<div class="col-sm-5 col-sm-offset-1 mobile-no-margin col-sm-push-6 stick-in-parent">
 					<img src="<?php the_sub_field('image'); ?>" alt="<?php the_field('project_title'); ?> by <?php the_title(); ?>">
 				</div>
-				<div class="col-sm-6 col-sm-pull-6 stick-in-parent">
-					<p class="caption"><?php the_sub_field('caption'); ?></p>
+				<div class="caption-box">
+					<div class="col-sm-6 col-sm-pull-6 stick-in-parent">
+						<p class="caption"><?php the_sub_field('caption'); ?></p>
+					</div>
 				</div>
 				<?php else : ?>
 				<div class="col-sm-5 col-sm-offset-7 mobile-no-margin stick-in-parent">
@@ -280,8 +282,10 @@
 					<img src="<?php the_sub_field('image'); ?>" alt="<?php the_field('project_title'); ?> by <?php the_title(); ?>">
 				</div>
 				<?php if(get_sub_field('caption')): ?>
-					<div class="col-sm-6 stick-in-parent caption-box bwb-push-6">
-						<p class="caption"><?php the_sub_field('caption'); ?></p>
+					<div class="caption-box">
+						<div class="col-sm-6 stick-in-parent bwb-push-6">
+							<p class="caption"><?php the_sub_field('caption'); ?></p>
+						</div>
 					</div>
 				<?php endif; ?>
 			</div>
@@ -297,9 +301,9 @@
 					<img src="<?php the_sub_field('image'); ?>" alt="<?php the_field('project_title'); ?> by <?php the_title(); ?>">
 				</div>
 				<div class="caption-box">
-				<div class="col-sm-6 col-sm-pull-6 stick-in-parent">
-					<p class="caption"><?php the_sub_field('caption'); ?></p>
-				</div>
+					<div class="col-sm-6 col-sm-pull-6 stick-in-parent">
+						<p class="caption"><?php the_sub_field('caption'); ?></p>
+					</div>
 				</div>
 				<?php else : ?>
 				<div class="col-sm-6 col-sm-offset-6 mobile-no-margin stick-in-parent">
