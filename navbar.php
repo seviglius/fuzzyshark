3a840beb2cf33c29878e62f769c008347a5164b2
mdebauge <mdebau@gmail.com>
2016-05-26 12:05:36 -0700

    switched to SVG index Navbar

--- navbar.php
+++ navbar.php
@@ -13,9 +13,71 @@
     <div class="container filters">
       <div class="row">
             <ul>
-              <a class="click active" id="all" href=#><li>All</li></a>
-              <a class="click" id="design" href=#><li>Design</li></a>
-              <a class="click" id="photography" href=#><li>Photo</li></a>
+              <a class="click active" id="all" href=#>
+                <li>
+                  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                     viewBox="0 0 70.5 52.9" style="enable-background:new 0 0 70.5 52.9;" xml:space="preserve">
+                  <g id="XMLID_158_">
+                    <path id="XMLID_159_" style="fill:none;stroke:#433A97;stroke-miterlimit:10;" d="M17.5,3.4h7.7l14.7,36.2h-8.5L28.5,32H13.9
+                      l-3,7.6H2.8L17.5,3.4z M16.4,25.8h9.8l-1.5-4c-1-2.9-2.4-6.5-3.3-9.2h-0.1c-1,2.8-2.2,6.4-3.4,9.4L16.4,25.8z"/>
+                    <path id="XMLID_162_" style="fill:none;stroke:#433A97;stroke-miterlimit:10;" d="M43.2,1.4h7.6v38.2h-7.6V1.4z"/>
+                    <path id="XMLID_164_" style="fill:none;stroke:#433A97;stroke-miterlimit:10;" d="M57.1,1.4h7.6v38.2h-7.6V1.4z"/>
+                  </g>
+                  </svg>
+                </li>
+              </a>
+              <a class="click" id="design" href=#>
+                <li>
+                  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                     viewBox="0 0 205.8 52.9" style="enable-background:new 0 0 205.8 52.9;" xml:space="preserve">
+                  <g id="XMLID_106_" style="enable-background:new    ;">
+                    <path id="XMLID_120_" style="fill:none;stroke:#433A97;stroke-miterlimit:10;" d="M23.2,2.6h10.2c6.5,0,11.5,1,15.2,3.7
+                      c4.5,3.1,7,8.2,7,14.4c0,5.7-2.1,10.6-6,13.7c-3.7,3-8.9,4.5-16.5,4.5h-9.8V2.6z M33.4,31.8c5.2,0,8.5-0.9,10.7-2.9
+                      c2.2-1.8,3.5-4.8,3.5-8.4c0-3.8-1.5-7-4.2-8.9c-2.6-1.8-6-2.2-10.1-2.2h-2.2v22.3H33.4z"/>
+                    <path id="XMLID_117_" style="fill:none;stroke:#433A97;stroke-miterlimit:10;" d="M66.5,28.5c0.9,3.4,3.7,5,7.2,5
+                      c2.8,0,5.8-1,8.4-2.7v6.4c-2.4,1.5-6.1,2.2-9.4,2.2c-8.1,0-14-5-14-12.8c0-7.8,6.1-13,12.9-13c6.1,0,11.8,4,11.8,12.3
+                      c0,0.8,0,2-0.1,2.6H66.5z M76.3,23.7c-0.4-2.7-2.4-4.3-4.9-4.3c-2.4,0-4.4,1.5-5.1,4.3H76.3z"/>
+                    <path id="XMLID_115_" style="fill:none;stroke:#433A97;stroke-miterlimit:10;" d="M87.6,30.6c3.4,2,7.4,2.8,9.7,2.8
+                      c2.1,0,3.4-0.5,3.4-1.9c0-1.9-2.8-1.7-6.5-2.7c-4.7-1.2-7.4-3.1-7.4-7.2c0.1-5.2,4.7-8.2,10.8-8.2c3,0,6.6,0.6,9.4,2v6.5
+                      c-3.2-1.8-7.1-2.6-9.3-2.6c-2,0-3.2,0.6-3.2,1.8c0,1.8,2.7,1.6,6.3,2.5c4.7,1.2,7.6,3,7.6,7.2c0,5.4-4.8,8.4-11.1,8.4
+                      c-3,0-6.8-0.6-9.7-2V30.6z"/>
+                    <path id="XMLID_112_" style="fill:none;stroke:#433A97;stroke-miterlimit:10;" d="M116.8,2c2.4,0,4.4,2,4.4,4.3
+                      c0,2.4-2,4.4-4.4,4.4s-4.4-2-4.4-4.4C112.4,4,114.4,2,116.8,2z M113,14.2h7.6v24.6H113V14.2z"/>
+                    <path id="XMLID_109_" style="fill:none;stroke:#433A97;stroke-miterlimit:10;" d="M128.9,41.3c2.5,1.6,5.4,2.5,8,2.5
+                      c3.7,0,6-1.9,6-5.6v-1.4l-0.1,0c-1.4,1.1-3.7,1.7-5.8,1.7c-6,0-11.5-4.6-11.5-12.1c0-7.2,5.2-12.9,12.8-12.9c2.4,0,4.4,0.5,6.2,1.4
+                      l6-1.1v24.3c0,7.8-5.3,12.1-13.2,12.1c-3.2,0-6.3-0.8-8.4-1.8V41.3z M142.8,21c-0.9-0.5-2.4-0.9-3.8-0.9c-3.9,0-5.8,2.9-5.8,6.1
+                      c0,3.6,2.2,6.2,5.8,6.2c1.3,0,2.8-0.4,3.8-1V21z"/>
+                    <path id="XMLID_107_" style="fill:none;stroke:#433A97;stroke-miterlimit:10;" d="M156.8,14.2h7.5v2.2l0.1,0
+                      c1.9-1.5,4.5-2.8,7.6-2.8c2.6,0,4.9,0.9,6.5,2.5c1.5,1.6,2.2,3.8,2.2,7.3v15.4H173V24.7c0-1.9-0.2-2.8-0.9-3.5
+                      c-0.6-0.8-1.5-1.1-2.6-1.1c-2,0-3.8,1-5.2,2.1v16.6h-7.6V14.2z"/>
+                  </g>
+                  </svg>
+                </li>
+              </a>
+              <a class="click" id="photography" href=#>
+                <li>
+                  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                     viewBox="0 0 144.4 52.9" style="enable-background:new 0 0 144.4 52.9;" xml:space="preserve">
+                  <g id="XMLID_120_">
+                    <path id="XMLID_121_" style="fill:none;stroke:#433A97;stroke-miterlimit:10;" d="M3.9,3.4h10.8c4.3,0,7.9,0.4,10.8,1.9
+                      c3.2,1.7,5.9,4.8,5.9,9.7c0,4.5-2.1,7.7-5.2,9.5c-2.8,1.6-6.4,2.2-11.7,2.2h-2.5v13h-8V3.4z M14.7,20.2c3.2,0,5-0.1,6.3-0.9
+                      c1.5-0.8,2.2-2.1,2.2-4c0-2.1-0.9-3.5-2.5-4.3c-1.5-0.6-3.4-0.8-6.2-0.8h-2.7v9.9H14.7z"/>
+                    <path id="XMLID_136_" style="fill:none;stroke:#433A97;stroke-miterlimit:10;" d="M35.7,1.4h7.6v15.8l0.1,0
+                      c1.8-1.5,4.4-2.8,7.5-2.8c2.6,0,4.9,0.9,6.5,2.5c1.5,1.6,2.1,3.8,2.1,7.3v15.4h-7.6V25.5c0-1.9-0.2-2.8-0.9-3.5
+                      c-0.6-0.8-1.5-1-2.6-1c-2,0-3.8,1-5.2,2.1v16.6h-7.6V1.4z"/>
+                    <path id="XMLID_138_" style="fill:none;stroke:#433A97;stroke-miterlimit:10;" d="M77.8,14.5c7.9,0,13.7,5.6,13.7,12.9
+                      s-5.9,12.9-13.7,12.9s-13.7-5.6-13.7-12.9S70,14.5,77.8,14.5z M77.8,33.6c3.5,0,6.1-2.8,6.1-6.3c0-3.5-2.7-6.3-6.1-6.3
+                      s-6.1,2.8-6.1,6.3C71.7,30.8,74.4,33.6,77.8,33.6z"/>
+                    <path id="XMLID_141_" style="fill:none;stroke:#433A97;stroke-miterlimit:10;" d="M112.2,38.8c-1.2,0.8-3.4,1.4-5.6,1.4
+                      c-2.5,0-4.9-0.8-6.4-2.4c-1.3-1.4-2.2-3.5-2.2-7.4v-9.1h-4V15h4V8.6l7.6-1.3V15h6.5v6.3h-6.5v8.3c0,2.4,0.5,4.2,2.9,4.2
+                      c1.2,0,2.7-0.5,3.7-1V38.8z"/>
+                    <path id="XMLID_143_" style="fill:none;stroke:#433A97;stroke-miterlimit:10;" d="M128.5,14.5c7.9,0,13.7,5.6,13.7,12.9
+                      s-5.9,12.9-13.7,12.9s-13.7-5.6-13.7-12.9S120.7,14.5,128.5,14.5z M128.5,33.6c3.5,0,6.1-2.8,6.1-6.3c0-3.5-2.7-6.3-6.1-6.3
+                      s-6.1,2.8-6.1,6.3C122.4,30.8,125.1,33.6,128.5,33.6z"/>
+                  </g>
+                  </svg>
+                </li>
+              </a>
             </ul>
       </div><!-- <div class="row"> -->
       <div id="parent">
